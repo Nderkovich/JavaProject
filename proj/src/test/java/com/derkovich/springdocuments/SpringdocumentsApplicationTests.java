c4a907cfdda3dc800de087ed9c52cc7c4d254cc6
Nderkovich <nikitaderkovich@gmail.com>
2020-10-29 21:35:18 +0300

    Added testing

--- proj/src/test/java/com/derkovich/springdocuments/SpringdocumentsApplicationTests.java
+++ proj/src/test/java/com/derkovich/springdocuments/SpringdocumentsApplicationTests.java
@@ -2,12 +2,15 @@ package com.derkovich.springdocuments;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 class SpringdocumentsApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void userDoesntExist() {
     }
 
 }
